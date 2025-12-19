a6b909f61189d7287d91ea33d25248581021086a
zalwagdani <71978382+zalwagdani@users.noreply.github.com>
2025-12-19 16:30:27 +0300

    Update base path for Media repository

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/media-page/' : '/',
-  // If your repo name is different, change '/media-page/' to '/your-repo-name/'
+  base: process.env.NODE_ENV === 'production' ? '/Media/' : '/',
+  // Repository name: Media
 })
